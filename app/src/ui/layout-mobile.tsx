2dbe899d7a4fef4e437a14710772efcee6f3ec2c
Derek Scruggs <me@derekscruggs.com>
2023-12-28 17:53:16 -0600

    fix to layout padding on mobile

--- app/src/ui/layout-mobile.tsx
+++ app/src/ui/layout-mobile.tsx
@@ -17,36 +17,36 @@ export default function LayoutMobile({ children }: { children: React.ReactNode }
   
   const btnColor = theme.palette.primary.main;
   return (
-      <Container maxWidth="sm" className='h-screen'>
-      <Box sx={{ my: 0 }} className="h-full">
-          <div className="flex flex-col justify-between h-full pb-4 px-4">
-              <div className="flex justify-end items-start mb-4 pt-2 pr-0">
-                  <SearchIcon fontSize="medium" className='mr-4' />
-                  <NotificationsNoneIcon fontSize="medium" className='mr-4' />
-                  <ForumOutlinedIcon fontSize="medium" className='mr-1' />
-              </div>
-              <div className="flex flex-col items-center">
-                  {children}
-              </div>
-              <div className="flex justify-between border border-1 border-red">
-                <Link to="/" >
-                  <HomeOutlinedIcon fontSize="large" className='cursor-pointer' />
-                </Link>
-                <Link to="/challenges" >
-                  <EmojiEventsOutlinedIcon fontSize="large" className='cursor-pointer' />
-                </Link>
-                <Link to="/new" className="-mt-6">
-                  <AddCircleOutlinedIcon style={{ fontSize: '3rem', color: `${btnColor}` }} className='color-main cursor-pointer text-6xl' />
-                </Link>
-                <Link to="/groups" >
-                  <PeopleOutlineOutlinedIcon fontSize='large' className='cursor-pointer' />
-                </Link>
-                <Link to="/profile" >
-                  <PortraitOutlinedIcon fontSize='large' className='cursor-pointer' />
-                </Link>
-              </div>
-          </div>
-      </Box>
+      <Container maxWidth="sm" className='h-screen !p-0 border-2 border-solid border-blue-500'>
+        <Box sx={{ my: 0 }} className="h-full p-0">
+            <div className="flex flex-col justify-between h-full p-0">
+                <div className="flex justify-end items-start mb-4 pt-2 pr-0">
+                    <SearchIcon fontSize="medium" className='mr-4' />
+                    <NotificationsNoneIcon fontSize="medium" className='mr-4' />
+                    <ForumOutlinedIcon fontSize="medium" className='mr-0' />
+                </div>
+                <div className="flex flex-col items-center">
+                    {children}
+                </div>
+                <div className="flex w-full justify-between border-solid border-2 border-red-500 m-0 p-0">
+                    <Link to="/" >
+                    <HomeOutlinedIcon fontSize="large" className='cursor-pointer' />
+                    </Link>
+                    <Link to="/challenges" >
+                    <EmojiEventsOutlinedIcon fontSize="large" className='cursor-pointer' />
+                    </Link>
+                    <Link to="/new" className="-mt-6">
+                    <AddCircleOutlinedIcon style={{ fontSize: '3rem', color: `${btnColor}` }} className='color-main cursor-pointer text-6xl' />
+                    </Link>
+                    <Link to="/groups" >
+                    <PeopleOutlineOutlinedIcon fontSize='large' className='cursor-pointer' />
+                    </Link>
+                    <Link to="/profile" >
+                    <PortraitOutlinedIcon fontSize='large' className='cursor-pointer' />
+                    </Link>
+                </div>
+            </div>
+        </Box>
       </Container>
     
   );
