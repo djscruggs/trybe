fc27b53a69e573bfd630d4b9f4dc5cf012b7d6f7
Derek Scruggs <me@derekscruggs.com>
2024-01-11 14:42:44 -0600

    typo fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,7 +11,7 @@ export const meta: MetaFunction = () => [
 export default function Index() {
   return (
     <>
-          <h1>I will be the welcome screen for non logged incurrentUsers</h1>
+          <h1>I will be the welcome screen for non logged in users</h1>
           <div className="ml-24 w-24 h-20 flex items-center justify-center flex-col text-darkgrey text-center mb-4 hover:bg-gray-300 p-2 rounded-lg">
           <Link to="/login" className='flex items-center flex-col'>
             <SunIcon className='h-8 w-8 cursor-pointer mb-1' />
