3a247bcec4f73fb60f07ba48d32bc43c1c5a96d1
Derek Scruggs <me@derekscruggs.com>
2024-01-29 10:58:20 -0600

    cleaned  up loading errors

--- app/routes/challenges.$id.tsx
+++ app/routes/challenges.$id.tsx
@@ -1,56 +1,45 @@
-import React, { useState, useEffect } from 'react';
 import {loadChallenge} from '~/utils/challenge.server'
-import { useLoaderData, useSearchParams } from '@remix-run/react';
+import { useLoaderData } from '@remix-run/react';
 import { requireCurrentUser } from "../utils/auth.server"
 import type {  ObjectData} from '~/utils/types.server'
 import { json, LoaderFunction } from "@remix-run/node"; 
 import type {  LoaderFunctionArgs } from "@remix-run/node"; // or cloudflare/deno
-import { Outlet } from '@remix-run/react';
-import { toast } from 'react-hot-toast';
 import { Link } from '@remix-run/react';
 
 export const loader: LoaderFunction = async ({ request, params }) => {
   const currentUser = await requireCurrentUser(request)
-  if(!params.challengeId){
+  if(!params.id){
     return null;
   }
 
-  console.log('loading from view with params', params)
-  const result = await loadChallenge(params.challengeId, currentUser?.id)
+  
+  const result = await loadChallenge(params.id, currentUser?.id)
   
   if(!result){
     console.log('retirning error')
     const error = {loadingError: 'Challenge not found'}
     return json(error)
   }
-  console.log('retirning oject')
   const data: ObjectData = {object: result} 
   console.log(data)
   return json(data)
 }
 export default function ViewChallenge() {
-  const [searchParams] = useSearchParams();
-  useEffect(() =>{
-    if(searchParams.get('success')){
-      toast.success('Challenge created')
-    }
-  },[])
-  
-  // console.log(params.get('success'))
   const data: ObjectData  = useLoaderData()
-  console.log(data)
+  if(!data){
+    return <p>No data.</p>
+  }
   if(data?.loadingError){
     return <h1>{data.loadingError}</h1>
+  } 
+  if(!data?.object){
+    return <p>Loading...</p>
   }
-  if(!data.object){
-    return '<p>Loading...</p>'
-  }
-  
   return (
     <>
     <h1>{data.object.name}</h1>
     <Link className='underline text-red' to = {`/challenges/edit/${data.object.id}`}>edit</Link>&nbsp;&nbsp;
-    <Outlet/>
+    
     </>
   );
 }
--- app/routes/challenges.edit.$id.tsx
+++ app/routes/challenges.edit.$id.tsx
@@ -1,23 +1,25 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from 'react';
 import ChallengeForm from '~/components/form-challenge';
 import {loadChallenge} from '~/utils/challenge.server'
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useRouteLoaderData, useFetcher } from '@remix-run/react';
 import { requireCurrentUser } from "../utils/auth.server"
 import type {ObjectData} from '~/utils/types.server'
 import { json, LoaderFunction } from "@remix-run/node"; 
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node"; // or cloudflare/deno
-import {loader as parentLoader} from './challenges.$challengeId'
+import {loader as challengeLoader} from './challenges.$id'
+// import { useRouteLoaderData } from '@remix-run/react';
 
 export const loader: LoaderFunction = async ({ request, params, context }) => {
- return parentLoader({ request, params, context })
+  return challengeLoader({ request, params, context })
 }
 export default function EditChallenge() {
   const data: ObjectData  = useLoaderData()
   if(data?.loadingError){
     return <h1>{data.loadingError}</h1>
   }
-  if(!data.object){
-    return '<p>Loading...</p>'
+   
+  if(!data?.object){
+    return <p>Loading.</p> 
   }
   
   
