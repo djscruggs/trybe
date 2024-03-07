cb7f5d857be7ab8a0215ad5472649dd578cbb114
Derek Scruggs <me@derekscruggs.com>
2024-03-06 18:01:18 -0600

    better layout of challenge view

--- app/routes/challenges.$id.tsx
+++ app/routes/challenges.$id.tsx
@@ -62,14 +62,20 @@ export default function ViewChallenge() {
   };
   return (
     <>
-    <h1>{data.object.name}</h1>
-    {data.object.userId === currentUser?.id && (
-      <>
-        <Link className='underline text-red' to = {`/challenges/edit/${data.object.id}`}>edit</Link>&nbsp;&nbsp;
-        <Link className='underline text-red' onClick={handleDelete} to = {`/challenges/edit/${data.object.id}`}>delete</Link>&nbsp;&nbsp;
-      </>
-    )}
+    
+   
     <div className={`max-w-sm border-2 border-${colorToClassName(data.object.color)} rounded-md p-4`}>
+    
+      <div className="mb-6 flex flex-col justify-center">
+        <h1 className='flex justify-center'>{data.object.name}</h1>
+        {data.object.userId === currentUser?.id && (
+          <div className="flex justify-center mt-2">
+            <Link className='underline text-red' to = {`/challenges/edit/${data.object.id}`}>edit</Link>&nbsp;&nbsp;
+            <Link className='underline text-red' onClick={handleDelete} to = {`/challenges/edit/${data.object.id}`}>delete</Link>&nbsp;&nbsp;
+          </div>
+        )}
+      </div>
+    
       <div className="mb-2 flex justify-center">
         {data.object.coverPhoto && <img src={data.object.coverPhoto} alt={`${data.object.name} cover photo`} className="max-w-full max-h-40 rounded-sm" />}
       </div>
