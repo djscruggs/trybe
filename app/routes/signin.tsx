3b86b47a6a48325d868766b11d76932bade7a4df
Derek Scruggs <me@derekscruggs.com>
2024-05-06 14:46:28 -0500

    Trying to use Clerk SignedIn and SignedOut instead of redirect

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -1,28 +1,37 @@
 import { type LoaderFunction, type LoaderFunctionArgs, redirect } from '@remix-run/node'
-import { SignIn } from '@clerk/clerk-react'
+import { Link } from '@remix-run/react'
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignIn,
+  SignedIn,
+  SignedOut
+} from '@clerk/clerk-react'
 import { getAuth } from '@clerk/remix/ssr.server'
 
 export const loader: LoaderFunction = async (args: LoaderFunctionArgs) => {
   const { userId } = await getAuth(args)
-  console.log('userId', userId)
-  if (userId) {
-    return redirect('/home')
-  }
+
   return null
 }
 
 export default function SignInPage (): JSX.Element {
   return (
     <div className="justify-center items-center flex flex-col gap-y-4 w-screen h-screen">
-
-      <SignIn
-      appearance={{
-        elements: {
-          formButtonPrimary:
-            'bg-red hover:bg-yellow text-sm normal-case'
-        }
-      }}
-      />
+      <SignedIn>
+        <h1>You are signed in!</h1>
+        <Link to="/home">Home</Link>
+      </SignedIn>
+      <SignedOut>
+        <SignIn
+        appearance={{
+          elements: {
+            formButtonPrimary:
+              'bg-red hover:bg-yellow text-sm normal-case'
+          }
+        }}
+        />
+      </SignedOut>
     </div>
   )
 }
